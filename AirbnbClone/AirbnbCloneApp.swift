385e7f05c3de83e027ebe8ba4f6ae12dcc75490a
keremoflu <keremoflu92@gmail.com>
2024-01-21 19:09:16 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by user on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
